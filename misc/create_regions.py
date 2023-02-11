5de966f2c083bd66b2414cc7596a78a93758783d
Tahya Weiss-Gibbons <weissgib@gra-login1.graham.sharcnet>
2023-02-10 19:13:57 -0500

    updated region script

--- misc/create_regions.py
+++ misc/create_regions.py
@@ -2,6 +2,7 @@
 create_regions.py
 author: Tahya Weiss-Gibbons (weissgib@ualberta.ca)
 date: June 2021
+updated: January 2022
 
 creates mask files for different regions on the ANHA4 grid
 """
@@ -15,7 +16,6 @@ import cartopy.feature as feature
 
 #first lets read in the mesh grid file
 mesh_file = '/mnt/storage4/tahya/model_files/ANHA4_mesh_mask.nc'
-#mesh_file = '/mnt/storage4/tahya/model_files/ANHA12_mesh_zgr.nc'
 mesh_data = xr.open_dataset(mesh_file)
 
 x = mesh_data.dims['x']
@@ -28,40 +28,27 @@ nav_lev = mesh_data['mbathy']
 
 #want to define the lat/lon boundaries of the regions we want
 
-#old definition
-#can_basin = {'lon':[-161, -120], 'lat': [70, 80]}
-can_basin = {'lon':[-170, -130], 'lat':[70.5,80.5]}
-eur_basin = {'lon':[80, 168], 'lat':[78, 88]}
+hb_coast = {'lon': [-83.77, -15.52], 'lat': [58.49, 84]}
+hudson_bay = {'lon':[-98, -76], 'lat':[50, 66]}
 caa = {'lon':[-127, -80], 'lat':[66, 80]}
-hudson_bay = {'lon':[-95, -66], 'lat':[50, 64]}
-can_shelf = {'lon':[-171, -127], 'lat':[68, 76]}
-siberian_shelf = {'lon': [132,177], 'lat':[68,76]}
-bering_strait = {'lon':[-180,-158], 'lat': [63,71]}
-cntrl_arctic = {'lon':[-122,22], 'lat':[84,89]}
-davis_strait = {'lon': [-78,-53], 'lat':[66,74]}
-lab_sea = {'lon':[-51,-23], 'lat':[43,59]}
-nares_strait = {'lon': [-80, -34], 'lat': [74, 84]}
-fram_strait = {'lon': [-41, -4], 'lat': [62, 81]}
-lab_current = {'lon': [-70, -48], 'lat': [41, 60]}
-spna = {'lon': [0,60], 'lat': [47, 65]}
+#bs_coast = {'lon':[248.91,124.27], 'lat':[64.7,80.5]}
+bs_coast = {'lon':[-167, -125], 'lat': [59, 72]}
+#east_coast = {'lon': [31.43, 131.81], 'lat':[64.35, 76.76]}
+norway_coast = {'lon': [-18.14, 41], 'lat':[52.25,71.66]}
+bs_east = {'lon':[140, 190], 'lat': [66,77]}
+laptev_sea = {'lon': [104, 142], 'lat': [68, 80]}
+kara_sea = {'lon': [53, 104], 'lat': [65, 79]}
 
 #now go through the grid and make surface masks
 
-mesh_data['cb_mask'] = mesh_data['tmask'].copy()
-mesh_data['eb_mask'] = mesh_data['tmask'].copy()
-mesh_data['caa_mask'] = mesh_data['tmask'].copy()
 mesh_data['hb_mask'] = mesh_data['tmask'].copy()
-mesh_data['ss_mask'] = mesh_data['tmask'].copy()
-mesh_data['cs_mask'] = mesh_data['tmask'].copy()
+mesh_data['caa_mask'] = mesh_data['tmask'].copy()
 mesh_data['bs_mask'] = mesh_data['tmask'].copy()
-mesh_data['ca_mask'] = mesh_data['tmask'].copy()
-mesh_data['ds_mask'] = mesh_data['tmask'].copy()
-mesh_data['ls_mask'] = mesh_data['tmask'].copy()
-mesh_data['ns_mask'] = mesh_data['tmask'].copy()
-mesh_data['fs_mask'] = mesh_data['tmask'].copy()
-mesh_data['lc_mask'] = mesh_data['tmask'].copy()
-mesh_data['spna_mask'] = mesh_data['tmask'].copy()
-
+mesh_data['ec_mask'] = mesh_data['tmask'].copy()
+#mesh_data['nc_mask'] = mesh_data['tmask'].copy()
+mesh_data['bs_east_mask'] = mesh_data['tmask'].copy()
+mesh_data['laptev_mask'] = mesh_data['tmask'].copy()
+mesh_data['kara_mask'] = mesh_data['tmask'].copy()
 mesh_data['all_masks'] = mesh_data['tmask'].copy()
 
 for i in range(x):
@@ -69,93 +56,67 @@ for i in range(x):
         ln = nav_lon[j,i]
         lt = nav_lat[j,i]
         lv = nav_lev[0,j,i] 
-		
-        if can_basin['lon'][0] <= ln <= can_basin['lon'][1]:
-            if can_basin['lat'][0] <= lt <= can_basin['lat'][1]:
-                if lv >= 28:
-                    mesh_data['cb_mask'][:,:,j,i] = 2
+	
+        """	
+        if hb_coast['lon'][0] <= ln <= hb_coast['lon'][1]:
+            if hb_coast['lat'][0] <= lt <= hb_coast['lat'][1]:
+                if lv <= 28:
+                    mesh_data['hb_mask'][:,:,j,i] = 2
                     mesh_data['all_masks'][:,:,j,i] = 2
-
-        if eur_basin['lon'][0] <= ln <= eur_basin['lon'][1]:
-            if eur_basin['lat'][0] <= lt <= eur_basin['lat'][1]:
-                if lv >= 35:
-                    mesh_data['eb_mask'][:,:,j,i] = 2
-                    mesh_data['all_masks'][:,:,j,i] = 3
-
-        if caa['lon'][0] <= ln <= caa['lon'][1]:
-            if caa['lat'][0] <= lt <= caa['lat'][1]:
-                if lv <= 31:
-                    mesh_data['caa_mask'][:,:,j,i] = 2
-                    mesh_data['all_masks'][:,:,j,i] = 4
-
+        """
         if hudson_bay['lon'][0] <= ln <= hudson_bay['lon'][1]:
             if hudson_bay['lat'][0] <= lt <= hudson_bay['lat'][1]:
                 mesh_data['hb_mask'][:,:,j,i] = 2
-                mesh_data['all_masks'][:,:,j,i] = 5
+                mesh_data['all_masks'][:,:,j,i] = 2
 
-        if siberian_shelf['lon'][0] <= ln <= siberian_shelf['lon'][1]:
-            if siberian_shelf['lat'][0] <= lt <= siberian_shelf['lat'][1]:
-                if lv <= 20:
-                    mesh_data['ss_mask'][:,:,j,i] = 2
+        if caa['lon'][0] <= ln <= caa['lon'][1]:
+            if caa['lat'][0] <= lt <= caa['lat'][1]:
+                if lv <= 25:
+                    mesh_data['caa_mask'][:,:,j,i] = 2
+                    mesh_data['all_masks'][:,:,j,i] = 3
+
+        #bering strait section is a special case
+        #it crosses the lon boundary in -180 to 180 case
+        #so going to convert to 0 360 to avoid the problem
+
+        #dln = ((360 + (ln % 360)) % 360)
+        if bs_coast['lon'][0] <= ln <= bs_coast['lon'][1]: 
+            if bs_coast['lat'][0] <= lt <= bs_coast['lat'][1]:
+               if lv <= 25:
+                   mesh_data['bs_mask'][:,:,j,i] = 2
+                   mesh_data['all_masks'][:,:,j,i] = 4
+
+        if bs_east['lon'][0] <= ln <= bs_east['lon'][1]:
+            if bs_east['lat'][0] <= lt <= bs_east['lat'][1]:
+                if lv <= 25:
+                    mesh_data['bs_east_mask'][:,:,j,i] = 2
+                    mesh_data['all_masks'][:,:,j,i] = 5
+
+        if laptev_sea['lon'][0] <= ln <= laptev_sea['lon'][1]:
+            if laptev_sea['lat'][0] <= lt <= laptev_sea['lat'][1]:
+                if lv <= 25:
+                    mesh_data['laptev_mask'][:,:,j,i] = 2
                     mesh_data['all_masks'][:,:,j,i] = 6
 
-        if can_shelf['lon'][0] <= ln <= can_shelf['lon'][1]:
-            if can_shelf['lat'][0] <= lt <= can_shelf['lat'][1]:
-                if lv <= 20:
-                    mesh_data['cs_mask'][:,:,j,i] = 2
+        if kara_sea['lon'][0] <= ln <= kara_sea['lon'][1]:
+            if kara_sea['lat'][0] <= lt <= kara_sea['lat'][1]:
+                if lv <= 25:
+                    mesh_data['kara_mask'][:,:,j,i] = 2
                     mesh_data['all_masks'][:,:,j,i] = 7
 
-        if bering_strait['lon'][0] <= ln <= bering_strait['lon'][1]:
-            if bering_strait['lat'][0] <= lt <= bering_strait['lat'][1]:
-                mesh_data['bs_mask'][:,:,j,i] = 2
-                mesh_data['all_masks'][:,:,j,i] = 8
 
-        if cntrl_arctic['lon'][0] <= ln <= cntrl_arctic['lon'][1]:
-            if cntrl_arctic['lat'][0] <= lt <= cntrl_arctic['lat'][1]:
-                mesh_data['ca_mask'][:,:,j,i] = 2
-                mesh_data['all_masks'][:,:,j,i] = 9
+#        if norway_coast['lon'][0] <= ln <= norway_coast['lon'][1]:
+#            if norway_coast['lat'][0] <= lt <= norway_coast['lat'][1]:
+#                if lv <= 25:
+#                    mesh_data['nc_mask'][:,:,j,i] = 2
+#                    mesh_data['all_masks'][:,:,j,i] = 8
 
-        if davis_strait['lon'][0] <= ln <= davis_strait['lon'][1]:
-            if davis_strait['lat'][0] <= lt <= davis_strait['lat'][1]:
-                if lv >= 22:
-                    mesh_data['ds_mask'][:,:,j,i] = 2
-                    mesh_data['all_masks'][:,:,j,i] = 10
 
-        if lab_sea['lon'][0] <= ln <= lab_sea['lon'][1]:
-            if lab_sea['lat'][0] <= lt <= lab_sea['lat'][1]:
-                if lv >= 35:
-                    mesh_data['ls_mask'][:,:,j,i] = 2
-                    mesh_data['all_masks'][:,:,j,i] = 11
-
-        if lab_current['lon'][0] <= ln <= lab_current['lon'][1]:
-            if lab_current['lat'][0] <= lt <= lab_current['lat'][1]:
-                if lv <= 35:
-                    mesh_data['lc_mask'][:,:,j,i] = 2
-                    mesh_data['all_masks'][:,:,j,i] = 12
-
-        if nares_strait['lon'][0] <= ln <= nares_strait['lon'][1]:
-            if nares_strait['lat'][0] <= lt <= nares_strait['lat'][1]:
-                mesh_data['ns_mask'][:,:,j,i] = 2
-                mesh_data['all_masks'][:,:,j,i] = 13
-
-        if fram_strait['lon'][0] <= ln <= fram_strait['lon'][1]:
-            if fram_strait['lat'][0] <= lt <= fram_strait['lat'][1]:
-                if lv <= 45:
-                    mesh_data['fs_mask'][:,:,j,i] = 2
-                    mesh_data['all_masks'][:,:,j,i] = 14
-
-        if spna['lon'][0] <= ln <= spna['lon'][1]:
-            if spna['lat'][0] <= lt <= spna['lat'][1]:
-                if lv >= 35:
-                    mesh_data['spna_mask'][:,:,j,i] = 2
-                    mesh_data['all_masks'][:,:,j,i] = 15
-
-        
 
 #remask the data with the bathymetry now, since we ignored that when finding our bounding boxes
 mesh_data = mesh_data.where(mesh_data.tmask ==1)
 
 #lets write the data we have to a netcdf file
 
-mesh_data.to_netcdf('new_region_mask.nc')
+mesh_data.to_netcdf('runoff_temp_regions_mask_all_mask.nc')
 
