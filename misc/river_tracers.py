71cd0cce674aad5e86d6c3dd99acc504eeba57c7
Tahya Weiss-Gibbons <weissgib@gra-login1.graham.sharcnet>
2023-08-01 16:37:43 -0400

    updated river tracer plotting

--- misc/river_tracers.py
+++ misc/river_tracers.py
@@ -12,10 +12,40 @@ import matplotlib
 matplotlib.use('Agg')
 ###----------------------------------###
 
+def read_files(runid, endyear, endmonth, endday, startyear=2002, startmonth=1, startday=5):
+    path = "/project/6007519/pmyers/ANHA4/ANHA4-"+runid+"-S/"
+    home_path = '/project/6007519/weissgib/plotting/data_files/anha4_files/'
+
+    start_time = datetime.date(startyear, startmonth, startday)
+
+    end_time = datetime.date(endyear, endmonth, endday)
+
+    #figure out all the dates we have model files
+    delta = end_time - start_time
+    times = []
+
+    i = 0
+    while i < delta.days+1:
+        t = start_time + datetime.timedelta(days=i)
+        if t.month == 2 and t.day == 29:
+            t = datetime.date(t.year, 3, 1)
+            i = i+6
+        else:
+            i = i+5
+        times.append(t)
+
+    #just need the T grid variables
+    mdl_files = []
+
+    for t in times:
+        mdl_files.append(path+"ANHA4-"+runid+"_y"+str(t.year)+"m"+str(t.month).zfill(2)+"d"+str(t.day).zfill(2)+"_gridT.nc")
+
+    return(mdl_files,times)
+
 def river_tracers(runid, endyear, endmonth, endday, startyear=2002, startmonth=1, startday=5):
-    figs_path = '/project/6007519/weissgib/plotting/figs/tracers/'
-    path = "/project/6007519/weissgib/ANHA4/ANHA4-"+runid+"-S/"
-    home_path = '/project/6007519/weissgib/plotting/'
+    figs_path = '/project/6007519/weissgib/plotting/tracers/'
+    path = "/project/6007519/pmyers/ANHA4/ANHA4-"+runid+"-S/"
+    home_path = '/project/6007519/weissgib/plotting/data_files/anha4_files/'
 
     start_time = datetime.date(startyear, startmonth, startday)
 
@@ -51,7 +81,7 @@ def river_tracers(runid, endyear, endmonth, endday, startyear=2002, startmonth=1
     mf.close()
 
     #tracers = ['TRC13', 'TRC14', 'TRC15', 'TRC16']
-    tracers = ['TRC15']
+    tracers = ['TRC13']
 
     #north pole stero projection
     land_50m = feature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='black', facecolor='gray', linewidth=0.5)
@@ -88,7 +118,7 @@ def river_tracers(runid, endyear, endmonth, endday, startyear=2002, startmonth=1
             verts = np.vstack([np.sin(theta), np.cos(theta)]).T
             circle = mpath.Path(verts * radius + center)
             ax.set_boundary(circle, transform=ax.transAxes)
-            p1 = ax.pcolormesh(nav_lon, nav_lat, trac, transform=ccrs.PlateCarree(), cmap='jet', vmin=0, vmax=0.5)
+            p1 = ax.pcolormesh(nav_lon, nav_lat, trac, transform=ccrs.PlateCarree(), cmap='viridis', vmin=0, vmax=1.5)
             ax_cb = plt.axes([0.92, 0.25, 0.015, 0.5])
             cb = plt.colorbar(p1,cax=ax_cb, orientation='vertical')
             ax.gridlines()
@@ -100,6 +130,78 @@ def river_tracers(runid, endyear, endmonth, endday, startyear=2002, startmonth=1
         mdl_file.close()
 
 
+def trac_diff(runids,endyear, endmonth, endday, startyear=2002, startmonth=1, startday=5):
+    home_path = '/project/6007519/weissgib/plotting/data_files/anha4_files/'
+    figs_path = '/project/6007519/weissgib/plotting/tracers/'
+
+    r1, times = read_files(runids[0], endyear, endmonth, endday)
+    r2, times = read_files(runids[1], endyear, endmonth, endday)
+
+    tracer = 'TRC13'
+
+    mesh_file = home_path+'ANHA4_mesh_mask.nc'
+    mf = nc.Dataset(mesh_file)
+
+    nav_lon = np.array(mf.variables['nav_lon'])
+    nav_lat = np.array(mf.variables['nav_lat'])
+
+    mf.close()
+
+    land_50m = feature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='black', facecolor='gray', linewidth=0.5)
+    projection = ccrs.NorthPolarStereo()
+    fig = plt.figure(figsize=(10,9))
+
+    for i in range(len(r1)):
+
+        c1 = nc.Dataset(r1[i])
+        c2 = nc.Dataset(r2[i])
+        tim = times[i]
+
+        trac1 = np.array(c1[tracer])[0,:,:,:]
+        miss_val = c1[tracer].missing_value
+
+        trac1[trac1==miss_val] = np.nan
+
+        #sum the tracer over the water column
+        trac1 = np.sum(trac1, axis=0)
+
+        trac2 = np.array(c2[tracer])[0,:,:,:]
+        miss_val = c2[tracer].missing_value
+
+        trac2[trac2==miss_val] = np.nan
+
+        #sum the tracer over the water column
+        trac2 = np.sum(trac2, axis=0)
+
+        diff = trac1-trac2
+
+        #now plot the difference
+        ax = plt.subplot(1, 1, 1, projection=projection)
+
+        ax.set_extent([-280, 80, 80, 35], crs=ccrs.PlateCarree())
+        ax.add_feature(land_50m, color=[0.8, 0.8, 0.8])
+        ax.coastlines(resolution='50m')
+
+        # Compute a circle in axes coordinates, which we can use as a boundary
+        # for the map. We can pan/zoom as much as we like - the boundary will be
+        # permanently circular.
+        theta = np.linspace(0, 2*np.pi, 100)
+        center, radius = [0.5, 0.5], 0.5
+        verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+        circle = mpath.Path(verts * radius + center)
+        ax.set_boundary(circle, transform=ax.transAxes)
+        p1 = ax.pcolormesh(nav_lon, nav_lat, diff, transform=ccrs.PlateCarree(), cmap='bwr', vmin=-1, vmax=1)
+        ax_cb = plt.axes([0.92, 0.25, 0.015, 0.5])
+        cb = plt.colorbar(p1,cax=ax_cb, orientation='vertical')
+        ax.gridlines()
+        plt.title(str(tim))
+        plt.savefig(figs_path+'diff_'+tracer+'_'+str(tim)+'.png')
+        #plt.show()
+        plt.clf()
+
+
 if __name__ == "__main__":
-        river_tracers(runid='ETW001', startyear=2002, endyear=2019, endmonth=6, endday=14)
+        river_tracers(runid='EPM151', startyear=2002, endyear=2019, endmonth=12, endday=31)
+        river_tracers(runid='EPM015', startyear=2002, endyear=2019, endmonth=12, endday=21)
+        #trac_diff(runids=['EPM151', 'EPM015'], endyear=2019, endmonth=12, endday=21)
 
