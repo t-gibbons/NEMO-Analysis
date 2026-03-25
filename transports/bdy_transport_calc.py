eee1134be7de6b47288887aa6a322c9899417764
Tahya Weiss-Gibbons <weissgib@l3.nibi.sharcnet>
2026-03-25 18:17:02 -0400

    code version consistent with ANHA4

--- transports/bdy_transport_calc.py
+++ transports/bdy_transport_calc.py
@@ -2,83 +2,115 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
-#we want to calculate the volume transport in the regional boundary files
-
-#bdy file
-bdy_path ='/project/6007519/ANHA4-I/BDY/GLORYS2V3/'
-
-bdy_file = bdy_path+'ANHA4_bdy_south_GLORYS2V3_y2002.nc'
-
-bf = xr.open_mfdataset(bdy_file)
-
-#also need the grid file to land mask
-
+#define your file paths here
+bdy_path = '/project/6007519/ANHA4-I/BDY/eORCA025-ECP013/'
+bath_file = '/project/6007519/weissgib/ANHA4-I/ANHA4_bathy_etopo1_gebco1_smoothed_coast_corrected_mar10.nc'
 grid_file = '/project/6007519/weissgib/ANHA4-I/ANHA4_mesh_mask.nc'
-
-gf = xr.open_mfdataset(grid_file)
-
-e2v = gf['e2v'][0, 0, :].values
-
-#get in the right shape
-e2v = np.tile(e2v.reshape(1, 1, 1, 544), (12, 50, 1, 1))
-
-e2v = e2v[:, :, :, 2:544]
-
-gf.close()
-
-#bt = xr.open_mfdataset('/project/6007519/weissgib/ANHA4-I/ANHA4_bathy.nc')
-bt = xr.open_mfdataset('/project/6007519/weissgib/ANHA4-I/ANHA4_bathy_etopo1_gebco1_smoothed_coast_corrected_mar10.nc')
-
-bath = bt['Bathymetry'][0, :].values
-bath = np.where(bath != 0, 1, 0)
-bath = np.tile(bath.reshape(1, 1, 1, 544), (12, 50, 1, 1))
-
-bath = bath[:, :, :, 2:544]
-
-bt.close()
-
-print(bath.shape)
-
-#and the cell thickness, can just use from anha4x
-
 thickness_file = '/project/6007519/weissgib/plotting/data_files/anha4_files/computed_u_thickness.npy'
+fig_dir = '/project/6007519/weissgib/plotting/transports/figs/'
 
-thickness = np.load(thickness_file)
+#we want to calculate the volume transport in the regional boundary files
+def calc_bdy_trans(year, direction):
 
-thickness = thickness[:,0,2:544]
+    if direction == 'north':
+        i = 799
+        j = 541
+    elif direction == 'south':
+        i = 0
+        j = 542
+    else:
+        print('Not a supported boundary')
+        exit()
 
-thickness = np.tile(thickness.reshape(1,50,1,542), (12, 1, 1, 1))
+    bdy_data = bdy_path+'ANHA4_bdy_'+direction+'_eORCA025-ECP013_y'+year+'.nc'
 
-print(thickness.shape)
+    bf = xr.open_mfdataset(bdy_data)
 
-#and now we can calculate the transport
+    print(bf)
 
-masked_vel = bf['vomecrty']*bath
+    #will need the grid file
 
-vol_trans = masked_vel*thickness*e2v
+    gf = xr.open_mfdataset(grid_file)
 
-print(vol_trans)
+    e2v = gf['e2v'][0, i, :].values
 
-#plt.imshow(vol_trans[0, :, 0, :])
-#plt.show()
-#exit()
+    #get in the right shape
+    e2v = np.tile(e2v.reshape(1, 1, 1, 544), (12, 50, 1, 1))
 
-#average the transport over depth and across the line
-trans = vol_trans.sum(dim='z')
-trans = trans.sum(dim='x')
+    e2v = e2v[:, :, :, 2:j+2]
 
-#convert to sverdrups
-trans = trans*0.000001
+    print(e2v.shape)
 
-print(trans)
-avg_trans = trans.mean('time_counter')
-print(avg_trans.values)
+    gf.close()
 
-plt.plot(trans[:,0])
-#plt.savefig('bdy_glorys_anha4_south.png')
-plt.show()
+    bt = xr.open_mfdataset(bath_file)
 
-bf.close()
+    bath = bt['Bathymetry'][i, :].values
+    bath = np.where(bath != 0, 1, 0)
+    bath = np.tile(bath.reshape(1, 1, 1, 544), (12, 50, 1, 1))
 
+    bath = bath[:, :, :, 2:j+2]
 
+    bt.close()
 
+    print(bath.shape)
+
+    #and final file is the cell thickness
+
+    thickness = np.load(thickness_file)
+
+    thickness = thickness[:,i,2:j+2]
+
+    thickness = np.tile(thickness.reshape(1,50,1,j), (12, 1, 1, 1))
+
+    print(thickness.shape)
+
+    #and now we can calculate the transport
+
+    print(bf['vomecrty'])
+
+    masked_vel = bf['vomecrty']*bath
+
+    vol_trans = masked_vel*thickness*e2v
+
+    print(vol_trans)
+
+    #average the transport over depth and across the line
+    trans = vol_trans.sum(dim='z')
+    trans = trans.sum(dim='x')
+
+    #convert to sverdrups
+    trans = trans*0.000001
+
+    return(trans)
+
+def plot_bdy_transport():
+
+    #get the bdy transport for both north and south
+    trans_north = calc_bdy_trans('2002', 'north')
+    trans_south = calc_bdy_trans('2003', 'south')
+
+    months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+    avg_trans_north = trans_north.mean('time_counter')
+    avg_trans_south = trans_south.mean('time_counter')
+    print("Average North BDY Transport: "+str(avg_trans_north.values[0]))
+    print("Average South BDY Transport: "+str(avg_trans_south.values[0]))
+
+    plt.plot(months, trans_north[:,0], color='green', label='North Transport')
+    plt.plot(months, trans_south[:,0], color='blue', label='South Transport')
+
+    plt.axhline(y=avg_trans_north, color='green', linestyle='dashed')
+    plt.axhline(y=avg_trans_south, color='blue', linestyle='dashed')
+
+    plt.legend()
+    plt.ylabel('transport (Sv)')
+    plt.title('Boundary Transport ANHA4 eORCA025-ECP013 2002')
+
+    plt.savefig(fig_dir+'bdy_trans_eORCA025-ECP013_anha4.png')
+    #plt.show()
+
+    trans_north.close()
+    trans_south.close()
+
+plot_bdy_transport()
